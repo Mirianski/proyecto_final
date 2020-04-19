5ce8e9fef09d50f430ba5dfc38cfdc24af9ce81b
Miriam <miriam.martinba93@gmail.com>
2020-04-19 19:09:25 +0100

    stage cahanges

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,7 @@
         $usuario = $_POST['user'];
         $contra = $_POST['password'];
 
-        $db = new mysqli("localhost", "root", "uniroot", "chefmi");
+        $db = new mysqli("localhost", "root", "", "chefmi");
         $db->set_charset("UTF8");
 
         if ($db->connect_error) {
